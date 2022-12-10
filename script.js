2658309ebcb457590e06345955a741e5590f1676
rjames187 <rory.james2021@gmail.com>
2022-12-09 20:16:10 -0600

    write render book procedure

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+// Organization
+
 let myLibrary = [];
 
 function Book(title, author, pages, read) {
@@ -13,3 +15,31 @@ function Book(title, author, pages, read) {
 function addBookToLibrary(title, author, pages, read) {
     myLibrary.push(new Book(title, author, pages, read));
 }
+
+// DOM Manipulation
+
+const main = document.querySelector("main");
+
+function renderBook(book) {
+    const card = document.createElement("div.card");
+
+    const title = document.createElement("h1");
+    title.textContent = book.title;
+    card.appendChild(title);
+
+    const author = document.createElement("div.author");
+    author.textContent = book.author
+    card.appendChild(author);
+
+    const pages = document.createElement("div.pages");
+    pages.textContent = book.pages;
+    card.appendChild(pages);
+
+    const read = document.createElement(book.read ? "button.read" : "button.not-read");
+    read.textContent = book.read ? "button.read" : "button.not-read";
+    card.appendChild(read);
+}
+
+// testing
+
+addBookToLibrary("Designing Data Intensive Applications", "Martin Kleppman", 661, false);
