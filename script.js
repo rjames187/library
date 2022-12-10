761af59679bee8174678ea054d0304f4036e5039
rjames187 <rory.james2021@gmail.com>
2022-12-09 19:37:47 -0600

    create book constructor

--- script.js
+++ script.js
@@ -1,1 +1,15 @@
-console.log("hello world!")
+let myLibrary = [];
+
+function Book(title, author, pages, read) {
+    this.title = title;
+    this.author = author;
+    this.pages = pages;
+    this.read = read;
+    this.info = function() {
+        console.log(`${this.title} by ${this.author}, ${this.pages}, ${this.read ? "read" : "not read yet"}`)
+    }
+}
+
+function addBookToLibrary(title, author, pages, read) {
+    myLibrary.push(new Book(title, author, pages, read));
+}
